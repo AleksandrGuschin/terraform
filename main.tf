5ade8f0cc98225fbddae8f997c5244d7cb9802d2
AleksandrGuschin <77740727+AleksandrGuschin@users.noreply.github.com>
2021-07-01 17:04:47 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+        region = "eu-central-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+data "aws_region""current" {
+}
+
+data "aws_caller_identity" "current" {
+}
